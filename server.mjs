ad332046099d55c84d894a046b92551d6070a711
AnnoudM <145171745+AnnoudM@users.noreply.github.com>
2023-12-04 01:02:05 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,9 +4,16 @@ import { hash , compare } from 'bcrypt';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import {signin,signup } from './db/db.mjs';
+import secret from './client_secret.json' assert {type : 'json'} ;
+import fetch from 'node-fetch';
+import querystring from 'querystring' ;  
 
 const { urlencoded } = bodyParser;
 const app = express();
+
+const CLIENT_ID = secret.client_id
+const CLIENT_SECRET = secret.client_secret
+const REDIRECT_URI = 'http://localhost:3000/auth/google/callback';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -29,20 +36,42 @@ app.post('/signup', async (req, res) => {
 });
 
 // Login endpoint
+
+
+
+
 app.post('/login', async (req, res) => {
-  
-    const { username, password } = req.body;
-    let user = signin(username);
-    if (!user) {
-      return res.status(400).send('Invalid credentials');
-    }
-    const match = await compare(password, user.password);
-    if (match) {
-      res.send('Login successful');
-    } else {
-      res.status(400).send('Invalid credentials');
-    }
+  const { username, password } = req.body;
+  let user = signin(username);
+  if (!user) {
+    return res.status(400).send('Invalid credentials');
+  }
+  const match = await compare(password, user.password);
+  if (match) {
+    res.send('Login successful');
+  } else {
+    res.status(400).send('Invalid credentials');
+  }
 });
+
+// Move Google authentication routes outside of the /login route
+app.get('/auth/google', (req, res) => {
+  const authorizationUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const params = {
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: 'code',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+    access_type: 'online'
+  };
+  res.redirect(`${authorizationUrl}?${querystring.stringify(params)}`);
+});
+
+app.get('/auth/google/callback', async (req, res) => {
+  // ... (Your existing Google authentication code)
+});
+
+// ... (Your existing signup route)
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
