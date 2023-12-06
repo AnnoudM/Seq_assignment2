9e39496a61a592e519ac3d80a9e2eb1f9656cb46
Riyam <88596299+RIYAM-M-S@users.noreply.github.com>
2023-12-06 23:09:55 +0300

    Github Button

--- public/auth.js
+++ public/auth.js
@@ -1,3 +1,7 @@
 document.getElementById('loginButton').addEventListener('click', () => {
     window.location.href = '/auth/google';
 });
+
+document.getElementById('loginGithub').addEventListener('click', () => {
+    window.location.href = '/auth/github';
+});
