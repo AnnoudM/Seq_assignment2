ed3a39a7b2c85c88f83691933087dff144621682
AnnoudM <145171745+AnnoudM@users.noreply.github.com>
2023-12-06 18:15:29 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,19 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { hash , compare } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import {signin,signup } from './db/db.mjs';
-import secret from './client_secret.json' assert {type : 'json'} ;
+import { signin, signup } from './db/db.mjs';
+import secret from './client_secret.json' assert {type: 'json'};
 import fetch from 'node-fetch';
-import querystring from 'querystring' ;  
+import querystring from 'querystring';
+import secret2 from './secret.json' assert {type: 'json'};
 
 const { urlencoded } = bodyParser;
 const app = express();
 
-const CLIENT_ID = secret.client_id
-const CLIENT_SECRET = secret.client_secret
-const REDIRECT_URI = 'http://localhost:3000/auth/google/callback';
+const CLIENT_ID2 = secret2.client_id;
+const CLIENT_SECRET2 = secret2.client_secret;
+
+const CLIENT_ID = secret.client_id;
+const CLIENT_SECRET = secret.client_secret;
+const GOOGLE_REDIRECT_URI = 'http://localhost:3000/auth/google/callback';
+const GITHUB_REDIRECT_URI = 'http://localhost:3000/auth/github/callback';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -50,12 +55,12 @@ app.post('/login', async (req, res) => {
   }
 });
 
-
+// Google OAuth2
 app.get('/auth/google', (req, res) => {
   const authorizationUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
   const params = {
     client_id: CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
+    redirect_uri: GOOGLE_REDIRECT_URI,
     response_type: 'code',
     scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
     access_type: 'online'
@@ -64,67 +69,27 @@ app.get('/auth/google', (req, res) => {
 });
 
 app.get('/auth/google/callback', async (req, res) => {
-  const { code } = req.query;
-
-  if (!code) {
-    return res.status(400).send('Code not found');
-  }
-
-  try {
-    // Exchange the received code for an access token
-    const tokenParams = {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      redirect_uri: REDIRECT_URI,
-      grant_type: 'authorization_code',
-      code,
-    };
-
-    const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: querystring.stringify(tokenParams),
-    });
-
-    if (!tokenResponse.ok) {
-      throw new Error('Failed to fetch access token');
-    }
-
-    const tokenData = await tokenResponse.json();
-
-    // Now you have the access token, you can use it to fetch user info or perform actions on behalf of the user
-    const { access_token } = tokenData;
-
-    // Fetch user info using the access token
-    const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
-
-    if (!userInfoResponse.ok) {
-      throw new Error('Failed to fetch user information');
-    }
-
-    const userInfo = await userInfoResponse.json();
-
-    // At this point, you have the user information available in the `userInfo` object
-    console.log('User Info:', userInfo);
-
-    // Here you can proceed with creating or authenticating the user in your system
-    // For example:
-    // const { email, name } = userInfo;
-    // Perform actions based on this information, like registering the user in your system or logging them in
-
-    res.send('Google authentication successful');
-  } catch (error) {
-    console.error('Error during Google authentication:', error);
-    res.status(500).send('Failed to authenticate via Google');
-  }
+  const{code} = req.query ;
+  if(!code){
+    return res.status(400).send('Code not found');  }
 });
 
+// GitHub OAuth2
+app.get('/auth/github', (req, res) => {
+  const authorizationUrl = 'https://github.com/login/oauth/authorize';
+  const params = {
+    client_id: CLIENT_ID2,
+    redirect_uri: GITHUB_REDIRECT_URI,
+    scope: 'read:user', // Adjust the scope based on your requirements
+  };
+  res.redirect(`${authorizationUrl}?${querystring.stringify(params)}`);
+});
+
+app.get('/auth/github/callback', async (req, res) => {
+  const{code} = req.query ;
+  if(!code){
+    return res.status(400).send('Code not found');  }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
